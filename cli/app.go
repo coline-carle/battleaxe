1256181a1d190eefc35db5e296dfe782e6143acf
Coline Carle <coline.carle@gmail.com>
2018-10-05 22:51:55 +0200

    bump version

--- cli/app.go
+++ cli/app.go
@@ -14,7 +14,7 @@ import (
 )
 
 // Version of the app
-const Version = "0.0.5"
+const Version = "0.0.6"
 
 // OauthTokenURL blizard oauth2 url to fetch token
 const OauthTokenURL = "https://us.battle.net/oauth/token"
